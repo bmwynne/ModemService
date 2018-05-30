b9fa509f25ca38803cb6bbb1f4148d98be664ec8
WilliamJohns <wjohns94@gmail.com>
2018-05-30 16:51:51 -0500

    removed residual .o files and modified header includes

--- mdm_service.c
+++ mdm_service.c
@@ -21,7 +21,7 @@ void mdm_loc_config(mdm_loc_config_t * config, mdm_cb_t * loc_config_cb);
 void mdm_loc_start(mdm_cb_t * loc_cb);
 void mdm_loc_stop(mdm_cb_t * loc_cb);
 
-void mod_tick() {
+void mdm_tick() {
     power_tick();
     cell_tick();
     gps_tick();
